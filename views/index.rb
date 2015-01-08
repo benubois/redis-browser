e4bdbed2e21b90ec3e4bddc9f91dca67de376b5b
Ben Ubois <ben@benubois.com>
2015-01-08 15:20:48 -0800

    Add ability to specify database number.

--- views/index.rb
+++ views/index.rb
@@ -1,15 +1,15 @@
 module RedisViewer
-  
+
   module Views
     class Index < Layout
-      
+
       def keys
-        $redis.keys("*").map{ |key| 
-          {:key => key, :type => $redis.type(key) } 
+        @redis.keys("*").map{ |key|
+          {:key => key, :type => @redis.type(key), :db => @db }
         }
       end
-      
+
     end
   end
-  
+
 end
--- views/show.rb
+++ views/show.rb
@@ -1,28 +1,28 @@
 module RedisViewer
   module Views
     class Show < Layout
-      
+
       def has_key
         @type == 'hash' || @type == 'zset'
       end
-      
+
       def data
         case @type
         when "string"
-          [{'value' => $redis[@key]}]
+          [{'value' => @redis[@key]}]
         when "list"
-          $redis.lrange(@key, 0, -1).map{ |value| {'value' => value} }
+          @redis.lrange(@key, 0, -1).map{ |value| {'value' => value} }
         when "set"
-          $redis.smembers(@key).map{ |value| {'value' => value} }
+          @redis.smembers(@key).map{ |value| {'value' => value} }
         when "hash"
-          $redis.hgetall(@key).map{ |key, value| {'key' => key, 'value' => value} }
+          @redis.hgetall(@key).map{ |key, value| {'key' => key, 'value' => value} }
         when "zset"
-          $redis.zrange(@key, 0, -1, {withscores: true}).map{ |(value, key)| {'key' => key, 'value' => value} }
+          @redis.zrange(@key, 0, -1, {withscores: true}).map{ |(value, key)| {'key' => key, 'value' => value} }
         else
           []
         end
       end
-      
+
     end
   end
 end
